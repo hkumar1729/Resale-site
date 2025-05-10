f02138ce32739a6c661f1a78ee38404c62e942ef
Harsh <harshk1729@gmail.com>
2025-05-10 17:28:24 +0530

    fix: improve responsive layout for mobile and desktop views

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,64 +1,144 @@
-import 'flowbite';
 import { useState } from 'react';
 
 const Contact = () => {
-    const [license, SetLicense] = useState('Licence type')
-    return (
-      <div id="contact" className="p-14">
-        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center">Contact Us</h2>
-            <p className="mb-8 lg:mb-16 font-light text-center sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
-            <form action="#" className="space-y-8">
-                <div>
-                    <label htmlFor="subject" className="block mb-2 text-sm font-medium">Your Name</label>
-                    <input type="text" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="John doe" required/>
-               </div>
-                <div>
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium">Email</label>
-                    <input type="email" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="e.g., johndoe@gmail.com" required/>
-                </div>
-                <div>
-                    <label htmlFor="subject" className="block mb-2 text-sm font-medium">Company</label>
-                    <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Company" required/>
-                </div>
-                
-                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-gray-700 dark:focus:ring-gray-800" type="button">{license}<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                </svg>
-                </button>
-                <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700">
-                    <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                      <li>
-                        <p className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=>{SetLicense('Business License')}}>Business License</p>
-                      </li>
-                      <li>
-                        <p className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=>{SetLicense('Educational License')}}>Educational License</p>
-                      </li>
-                      <li>
-                        <p className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=>{SetLicense('Open Source License')}}>Open Source License</p>
-                      </li>
-                      <li>
-                        <p className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=>{SetLicense('Subscription License')}}> Subscription License</p>
-                      </li>
-                    </ul>
-                </div>
+  const [license, setLicense] = useState('Licence type');
+  const [showDropdown, setShowDropdown] = useState(false);
 
-                <div className="sm:col-span-2">
-                    <label htmlFor="message" className="block mb-2 text-sm font-medium">Your message</label>
-                    <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
-                </div>
-                <button
-                  type="submit"
-                  className="text-white py-3 px-5 text-sm font-medium text-center rounded-lg bg-blue-700 sm:w-fit hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
-                >
-                  Send message
-                </button>
+  return (
+    <div id="contact" className="p-14">
+      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+        <h2 className="mb-14 text-4xl tracking-tight font-extrabold text-center">Contact Us</h2>
 
-            </form>
-        </div>
+        <form className="space-y-8">
+          <div>
+            <label htmlFor="subject" className="block mb-2 text-sm font-medium">Your Name</label>
+            <input
+              type="text"
+              id="email"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              placeholder="John doe"
+              required
+            />
+          </div>
+
+          <div>
+            <label htmlFor="email" className="block mb-2 text-sm font-medium">Email</label>
+            <input
+              type="email"
+              id="subject"
+              className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              placeholder="e.g., johndoe@gmail.com"
+              required
+            />
+          </div>
+
+          <div>
+            <label htmlFor="subject" className="block mb-2 text-sm font-medium">Company</label>
+            <input
+              type="text"
+              id="subject"
+              className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              placeholder="Company"
+              required
+            />
+          </div>
+
+          <div className="relative inline-block">
+            <button
+              onClick={() => setShowDropdown(!showDropdown)}
+              className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-gray-700 dark:focus:ring-gray-800"
+              type="button"
+            >
+              {license}
+              <svg
+                className="w-2.5 h-2.5 ms-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+
+            {showDropdown && (
+              <div className="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 mt-2">
+                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+                  <li>
+                    <p
+                      onClick={() => {
+                        setLicense('Business License');
+                        setShowDropdown(false);
+                      }}
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
+                    >
+                      Business License
+                    </p>
+                  </li>
+                  <li>
+                    <p
+                      onClick={() => {
+                        setLicense('Educational License');
+                        setShowDropdown(false);
+                      }}
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
+                    >
+                      Educational License
+                    </p>
+                  </li>
+                  <li>
+                    <p
+                      onClick={() => {
+                        setLicense('Open Source License');
+                        setShowDropdown(false);
+                      }}
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
+                    >
+                      Open Source License
+                    </p>
+                  </li>
+                  <li>
+                    <p
+                      onClick={() => {
+                        setLicense('Subscription License');
+                        setShowDropdown(false);
+                      }}
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
+                    >
+                      Subscription License
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+
+          <div className="sm:col-span-2">
+            <label htmlFor="message" className="block mb-2 text-sm font-medium">Your message</label>
+            <textarea
+              id="message"
+              rows={6}
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              placeholder="Leave a comment..."
+            ></textarea>
+          </div>
+
+          <button
+            type="submit"
+            className="text-white py-3 px-5 text-sm font-medium text-center rounded-lg bg-blue-700 sm:w-fit hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+          >
+            Send message
+          </button>
+        </form>
       </div>
-    );
-  };
-  
-  export default Contact;
-  
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -9,22 +9,29 @@ const HowItWorks = () => {
         id="working"
         className="flex flex-col items-center justify-center h-screen p-10 font-futura"
       >
-        <h1 className='p-5 mb-8 font-semibold text-3xl underline'>How it works</h1>
+        <h1 
+          className='p-5 mb-8 font-semibold text-3xl underline'>
+          How it works
+        </h1>
         <div className='flex flex-row items-center justify-center'>
-          <div className="text-center items-center mr-5">
-          <img src={uploadLicense} className='h-24 w-24'/>
-          <h1 className='font-medium mt-5'>Upload license</h1>
-          </div>
-          <div><img src={arrow} className='h-10 w-10 mb-8'/></div>
-          <div className="text-center mr-5 ml-5">
-            <img src={valuation} className='h-24 w-24'/>
-            <h1 className='font-medium mt-5'>Get Valuation</h1>
-          </div>
-          <div><img src={arrow} className='h-10 w-10 mb-8'/></div>
-          <div className="text-center ml-5">
-            <img src={getPaid} className='h-24 w-24'/>
-            <h1 className='font-medium mt-5'>Get Paid</h1>
-          </div>
+            <div className="text-center items-center mr-5">
+              <img src={uploadLicense} className='h-24 w-24'/>
+              <h1 className='font-medium mt-5'>Upload license</h1>
+            </div>
+            <div>
+              <img src={arrow} className='h-10 w-10 mb-8'/>
+            </div>
+            <div className="text-center mr-5 ml-5">
+              <img src={valuation} className='h-24 w-24'/>
+              <h1 className='font-medium mt-5'>Get Valuation</h1>
+            </div>
+            <div>
+              <img src={arrow} className='h-10 w-10 mb-8'/>
+            </div>
+            <div className="text-center ml-5">
+              <img src={getPaid} className='h-24 w-24'/>
+              <h1 className='font-medium mt-5'>Get Paid</h1>
+            </div>
         </div>
       </div>
     );
